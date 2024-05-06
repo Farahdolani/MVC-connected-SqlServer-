625581a7a663b4dd66e06da03ad03e28167a8e3a
dolanifarah <farahdolani88@gmail.com>
2024-05-06 18:25:33 +0300

    first

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using WebApplication3.Data;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            AplicationContext  context = new AplicationContext();
+            var employee = context.Employees.ToList();
+            return View(employee );
+        }
+    }
+}
---
+++ WebApplication3/Data/AplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class AplicationContext : DbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=.;database=asp2;trusted_connection=true;TrustServerCertificate=True;");
+        }
+    }
+}
